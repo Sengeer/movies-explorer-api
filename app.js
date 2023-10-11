3e647bc1d2492dbcb077fdff43fdb29a25d86c3d
Sengeer <sengeer@mail.ru>
2023-10-11 14:35:23 +0300

    feat: cоздать схемы и модели user, movie

---
+++ app.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const mongoose = require('mongoose');
+const cors = require('cors');
+// const { requestLogger } = require('./middlewares/logger');
+require('dotenv').config();
+// const errorer = require('./middlewares/errorer');
+
+const {
+  PORT = 3000,
+  URL = 'mongodb://127.0.0.1:27017/bitfilmsdb',
+} = process.env;
+const app = express();
+
+mongoose.connect(URL, {
+  useNewUrlParser: true,
+});
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+  ],
+  credentials: true,
+  maxAge: 30,
+}));
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+// app.use(requestLogger);
+
+// app.use('/', require('./routes/index'));
+
+// app.use(errorer);
+
+app.listen(PORT);
---
+++ model/movie.js
@@ -0,0 +1,75 @@
+const mongoose = require('mongoose');
+
+const movieSchema = new mongoose.Schema({
+  country: {
+    type: String,
+    required: true,
+  },
+  director: {
+    type: String,
+    required: true,
+  },
+  duration: {
+    type: Number,
+    required: true,
+  },
+  year: {
+    type: String,
+    required: true,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  image: {
+    type: String,
+    required: true,
+    validate: {
+      validator(link) {
+        return /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/.test(link);
+      },
+      message: 'Переданы некорректные данные',
+    },
+  },
+  trailerLink: {
+    type: String,
+    required: true,
+    validate: {
+      validator(link) {
+        return /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/.test(link);
+      },
+      message: 'Переданы некорректные данные',
+    },
+  },
+  thumbnail: {
+    type: String,
+    required: true,
+    validate: {
+      validator(link) {
+        return /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_+.~#?&/=]*)$/.test(link);
+      },
+      message: 'Переданы некорректные данные',
+    },
+  },
+  owner: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'user',
+    required: true,
+  },
+  movieId: {
+    type: Number,
+    required: true,
+  },
+  nameRU: {
+    type: String,
+    required: true,
+  },
+  nameEN: {
+    type: String,
+    required: true,
+  },
+}, {
+  versionKey: false,
+});
+
+module.exports = mongoose.model('movie', movieSchema);
